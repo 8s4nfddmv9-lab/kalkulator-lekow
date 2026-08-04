a84750c0b9123b2dbc5c3ade1aa2c469ab86f3e7
8s4nfddmv9-lab <8s4nfddmv9@privaterelay.appleid.com>
2026-08-04 11:46:43 +0200

    feat: wire the web analytics tracker at startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:kalkulator_lekow/app.dart';
+import 'package:kalkulator_lekow/infrastructure/analytics/analytics_factory.dart';
 import 'package:kalkulator_lekow/infrastructure/preferences/shared_preferences_calculator_preferences_store.dart';
 import 'package:kalkulator_lekow/infrastructure/pwa_install/shared_preferences_pwa_install_prompt_store.dart';
 
@@ -9,6 +10,7 @@ void main() {
     KalkulatorLekowApp(
       preferencesStore: SharedPreferencesCalculatorPreferencesStore(),
       pwaInstallPromptStore: SharedPreferencesPwaInstallPromptStore(),
+      analyticsTracker: createAnalyticsTracker(),
     ),
   );
 }
